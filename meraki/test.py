334f332ab23b8ce4f1b641f053034023a56907ff
Paul Knoll <itspaulknoll@gmail.com>
2023-02-07 11:34:46 -0500

    Chnages and test commit of changes

--- meraki/test.py
+++ meraki/test.py
@@ -1,4 +1,5 @@
 # Be sure to export your API key first
+# API_KEY=ehg549g5hilull4w7985go
 import meraki
 dashboard = meraki.DashboardAPI(API_KEY)
 my_orgs = dashboard.organizations.getOrganizations()
