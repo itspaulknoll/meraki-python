1049f68b130f436d7be32a5ca04324e0eaf3bb6c
Paul Knoll <itspaulknoll@gmail.com>
2023-02-07 12:35:30 -0500

    commit

--- meraki/test.py
+++ meraki/test.py
@@ -1,5 +1,7 @@
-# Be sure to export your API key first
+# Be sure to export your API key first from the shell
+# Do not put it in source code
 # API_KEY=ehg549g5hilull4w7985go
+# refer to https://developer.cisco.com/meraki/api-latest/
 import meraki
 dashboard = meraki.DashboardAPI(API_KEY)
 my_orgs = dashboard.organizations.getOrganizations()
