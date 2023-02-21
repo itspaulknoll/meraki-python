13dc53583397c9b5577bce2ec1f1df3b232238fd
Paul Knoll <itspaulknoll@gmail.com>
2023-02-21 16:35:03 -0500

    changes

--- chatgpt/baselines.py
+++ chatgpt/baselines.py
@@ -1,6 +1,7 @@
 import wx
 import os
 import subprocess
+import threading
 
 class NetworkMetrics(wx.Frame):
     def __init__(self, *args, **kw):
@@ -10,75 +11,111 @@ class NetworkMetrics(wx.Frame):
         self.InitUI()
         
     def InitUI(self):
-        # Set up main panel
+    # Set up main panel
         panel = wx.Panel(self)
 
-        # Add output text control
-        self.output = wx.TextCtrl(panel, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        # Set up splitter window
+        splitter = wx.SplitterWindow(panel, style=wx.SP_LIVE_UPDATE)
 
-        # Add interface button
-        intf_btn = wx.Button(panel, label="Interfaces")
+        # Set up left panel for buttons
+        left_panel = wx.Panel(splitter)
+        intf_btn = wx.Button(left_panel, label="Interfaces")
         intf_btn.Bind(wx.EVT_BUTTON, self.OnInterfaces)
-
-        # Add ping button
-        ping_btn = wx.Button(panel, label="Ping")
+        ping_btn = wx.Button(left_panel, label="Ping")
         ping_btn.Bind(wx.EVT_BUTTON, self.OnPing)
-
-        # Add speedtest button
-        speedtest_btn = wx.Button(panel, label="Speedtest")
+        speedtest_btn = wx.Button(left_panel, label="Speedtest")
         speedtest_btn.Bind(wx.EVT_BUTTON, self.OnSpeedtest)
-
-        # Add wireless environment info button
-        wifi_btn = wx.Button(panel, label="Wireless Environment")
+        wifi_btn = wx.Button(left_panel, label="Wireless Environment")
         wifi_btn.Bind(wx.EVT_BUTTON, self.OnWifi)
-
-        # Add quit button
-        quit_btn = wx.Button(panel, label="Quit")
+        loadtest_btn = wx.Button(left_panel, label="Load Test")
+        loadtest_btn.Bind(wx.EVT_BUTTON, self.OnLoadTest)
+        quit_btn = wx.Button(left_panel, label="Quit")
         quit_btn.Bind(wx.EVT_BUTTON, self.OnQuit)
+        left_sizer = wx.BoxSizer(wx.VERTICAL)
+        left_sizer.AddSpacer(5)
+        left_sizer.Add(intf_btn, 0, wx.EXPAND | wx.ALL, 5)
+        left_sizer.AddSpacer(5)
+        left_sizer.Add(ping_btn, 0, wx.EXPAND | wx.ALL, 5)
+        left_sizer.AddSpacer(5)
+        left_sizer.Add(speedtest_btn, 0, wx.EXPAND | wx.ALL, 5)
+        left_sizer.AddSpacer(5)
+        left_sizer.Add(wifi_btn, 0, wx.EXPAND | wx.ALL, 5)
+        left_sizer.AddSpacer(5)
+        left_sizer.Add(loadtest_btn, 0, wx.EXPAND | wx.ALL, 5)
+        left_sizer.AddSpacer(5)
+        left_sizer.Add(quit_btn, 0, wx.EXPAND | wx.ALL, 5)
+        left_panel.SetSizer(left_sizer)
+
+        # Set up right panel for output
+        right_panel = wx.Panel(splitter)
+        self.output = wx.TextCtrl(right_panel, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        right_sizer = wx.BoxSizer(wx.VERTICAL)
+        right_sizer.Add(self.output, 1, wx.EXPAND | wx.ALL, 5)
+        right_panel.SetSizer(right_sizer)
+
+        # Set up splitter window
+        splitter.SplitVertically(left_panel, right_panel, 200)
 
         # Set up sizer for main panel
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.output, 1, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(intf_btn, 0, wx.ALL, 5)
-        sizer.Add(ping_btn, 0, wx.ALL, 5)
-        sizer.Add(speedtest_btn, 0, wx.ALL, 5)
-        sizer.Add(wifi_btn, 0, wx.ALL, 5)
-        sizer.Add(quit_btn, 0, wx.ALL, 5)
+        sizer.Add(splitter, 1, wx.EXPAND | wx.ALL, 5)
 
         # Set sizer for panel and show UI
         panel.SetSizer(sizer)
         self.Show(True)
 
     def OnInterfaces(self, e):
-        # Get network interface information
-        result = subprocess.run(['ifconfig'], stdout=subprocess.PIPE)
-        output = result.stdout.decode()
+    # Define function to run ifconfig command
+        self.output.SetValue("Running a command please wait...")
+        def run_ifconfig():
+            result = subprocess.run(['ifconfig'], stdout=subprocess.PIPE)
+            output = result.stdout.decode()
+            wx.CallAfter(self.output.SetValue, output) # Update UI with output
 
-        # Set output text
-        self.output.SetValue(output)
-
+    # Start thread to run ifconfig command
+        thread = threading.Thread(target=run_ifconfig)
+        thread.start()
+    
     def OnPing(self, e):
         # Prompt user for ping address
+        self.output.SetValue("Running a command please wait...")
         address = wx.GetTextFromUser('Enter IP or address to ping:', 'Ping', '')
-
         # Execute ping command and set output text
         result = subprocess.run(['ping', '-c', '4', address], stdout=subprocess.PIPE)
         output = result.stdout.decode()
         self.output.SetValue(output)
 
     def OnSpeedtest(self, e):
-        # Execute speedtest command and set output text
-        result = subprocess.run(['speedtest-cli'], stdout=subprocess.PIPE)
-        output = result.stdout.decode()
-        self.output.SetValue(output)
+    # Define function to run speedtest command
+        def speed_test():
+            wx.CallAfter(self.output.SetValue, "Running a command please wait...") # Update UI with status message
+            result = subprocess.run(['speedtest-cli'], stdout=subprocess.PIPE)
+            output = result.stdout.decode()
+            wx.CallAfter(self.output.SetValue, output) # Update UI with output
+
+    # Start thread to run command
+        thread = threading.Thread(target=speed_test)
+        thread.start()
 
     def OnWifi(self, e):
+        self.output.SetValue("Running a command please wait...")
         # Get wireless environment information
         result = subprocess.run(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'], stdout=subprocess.PIPE)
         output = result.stdout.decode()
 
         # Set output text
         self.output.SetValue(output)
+
+    def OnLoadTest(self, e):
+        def load_test():
+        # Execute load/stress test command and set output text
+            wx.CallAfter(self.output.SetValue, "Running a command please wait...") # Update UI with status message
+            result = subprocess.run(['ping', '-c', '100', '-i', '0.1', '1.1.1.1'], stdout=subprocess.PIPE)
+            output = result.stdout.decode()
+            wx.CallAfter(self.output.SetValue, output) # Update UI with output
+    # Start thread to run command
+        thread = threading.Thread(target=load_test)
+        thread.start()
 
     def OnQuit(self, e):
         self.Close()
